2b50650a1a8897ff105bd6b41724dba200af68f8
LuciusWan <18099488938@163.com>
2024-12-19 12:40:22 +0800

    Add files via upload

---
+++ AITest1/src/main/java/com/test/aitest1/AiTest1Application.java
@@ -0,0 +1,13 @@
+package com.test.aitest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiTest1Application.class, args);
+    }
+
+}
